3fd6ce01c8c6b1661cdb9b3f9fdd1ea145214d57
jpimenteljose <jpimentel.jose@hotmail.com>
2021-12-16 17:20:21 -0300

    versão inicial do projeto

---
+++ ProjetoAula05_/Entities/Empresa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoAula05_.Entities
+{
+    public class Empresa
+    {
+        //propriedades -> prop + 2x[tab]
+        public Guid IdEmpresa { get; set; }
+        public string RazaoSocial { get; set; }
+        public string Cnpj { get; set; }
+        
+        //Relacionamento de Associação
+        //Empresa TEM MUITOS Funcionarios
+        public List<Funcionario> Funcionarios { get; set; }
+    }
+}
---
+++ ProjetoAula05_/Program.cs
@@ -0,0 +1,54 @@
+﻿using ProjetoAula05_.Controllers;
+using System;
+
+namespace ProjetoAula05_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nSISTEMA DE CONTROLE DE EMPRESAS E FUNCIONÁRIOS\n");
+
+            var empresaController = new EmpresaController();
+
+            Console.WriteLine("(1) Cadastrar empresa");
+            Console.WriteLine("(2) Atualizar empresa");
+            Console.WriteLine("(3) Excluir empresa");
+            Console.WriteLine("(4) Consultar empresas");
+            Console.WriteLine("(5) Sair");
+
+            try
+            {
+                Console.Write("\nEscolha a opção desejada: ");
+                var opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        empresaController.CadastrarEmpresa();
+                        Main(args); //recursividade
+                        break;
+                    case 2:
+                        empresaController.AtualizarEmpresa();
+                        Main(args); //recursividade
+                        break;
+                    case 3:
+                        empresaController.ExcluirEmpresa();
+                        Main(args); //recursividade
+                        break;
+                    case 4:
+                        empresaController.ConsultarEmpresas();
+                        Main(args); //recursividade
+                        break;
+                    case 5:
+                        break;
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nErro: " + e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
